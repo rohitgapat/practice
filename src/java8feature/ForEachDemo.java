bf78ff51224bb4dee9c921dcf8b5bc1998870c89
rohitgapat <rohitgapat233@gmail.com>
2025-12-17 00:00:14 +0530

    Java8Featurs updated

--- src/java8feature/ForEachDemo.java
+++ src/java8feature/ForEachDemo.java
@@ -9,27 +9,41 @@ public class ForEachDemo {
 	public void itarateArrayList(ArrayList<String> names) {
 		names.forEach(name ->  {System.out.println(name);});
 	}
-	
+
 	public void itarateMap(Map<String, Integer> m1) {
 		m1.forEach((key,value) -> {System.out.println(key+" = "+value);});
+	}
+
+	public void itarateKeysUsingLambda(Map<String, Integer> m1) {
+		m1.keySet().forEach(key -> {System.out.println(key);});
+	}
+
+	public void itarateValuesUsingLambda(Map<String, Integer> m1) {
+		m1.values().forEach(value -> {
+			if(value>2){
+				System.out.println(value);
+			}
+		});
 	}
 	
 	public static void main (String [] arg) {
 		ForEachDemo fe = new ForEachDemo();
 		ArrayList<String> name = new ArrayList<String>();
 		Map<String, Integer> m1 = new HashMap<String, Integer>();
-		
+
 		m1.put("a", 1);
 		m1.put("b", 2);
 		m1.put("c", 3);
 		m1.put("d", 4);
-		
+
 		name.add("rohit");
 		name.add("yogesh");
 		name.add("rahul");
 		name.add("Amit");
-		
+
 		fe.itarateArrayList(name);
 		fe.itarateMap(m1);
+		fe.itarateKeysUsingLambda(m1);
+		fe.itarateValuesUsingLambda(m1);
 	}
 }
